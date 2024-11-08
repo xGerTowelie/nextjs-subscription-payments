90b171609400deb8566953a2a46f679945675bf8
towelie <mail@towelie.dev>
2024-11-08 18:32:02 +0100

    update

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -27,6 +27,7 @@ export async function GET(request: NextRequest) {
     // URL to redirect to after sign in process completes
     return NextResponse.redirect(
         getStatusRedirect(
+            // TODO: fix
             `http://authtest.towelie.dev/account`,
             'Success!',
             'You are now signed in.'
