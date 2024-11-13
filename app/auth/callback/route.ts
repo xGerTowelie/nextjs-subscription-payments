959f4492905e2e5bb447a5d3c8dd56146f596832
towelie <mail@towelie.dev>
2024-11-13 20:37:18 +0100

    update

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,7 +6,7 @@ import { getErrorRedirect, getStatusRedirect } from '@/utils/helpers';
 export async function GET(request: NextRequest) {
     // The `/auth/callback` route is required for the server-side auth flow implemented
     // by the `@supabase/ssr` package. It exchanges an auth code for the user's session.
-    const requestUrl = new URL(request.url);
+    const requestUrl = new URL(request.nextUrl);
     const code = requestUrl.searchParams.get('code');
 
     if (code) {
